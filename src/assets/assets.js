5c4c7aca4ff0319636994b5eb57acb51c7466811
arbaz9234 <arbaz9234@gmail.com>
2026-04-06 14:35:32 +0530

    Added product explore modal

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -9,6 +9,7 @@ import logoIcon from "./logo-icon.svg";
 import logoText from "./logo-text.svg";
 import product1 from "./product1.jpg";
 import flipIcon from "./flip-icon.svg";
+
 export const assets = {
   logo,
   favicon,
@@ -30,46 +31,76 @@ export const products = [
     description:
       "A calming blend of French lavender and vanilla bean, perfect for unwinding.",
     price: 100,
-    image: [bannerImage1],
+    image: [bannerImage1, bannerImage2, bannerImage3, product1],
+    features: [
+      "Over 50 hours of long-lasting burn time",
+      "Eco-friendly natural soy wax blend",
+      "Subtle, sophisticated amber and wood fragrance",
+    ],
   },
   {
     id: "2",
-    name: "Lavender Dream",
+    name: "Enchanted Forest Amber",
     description:
-      "A calming blend of French lavender and vanilla bean, perfect for unwinding.",
-    price: 100,
-    image: [bannerImage1],
+      "Immerse yourself in the serene embrace of an enchanted forest. This exquisite candle blends rich amber notes with hints of warm sandalwood and crisp cedar, evoking a grounding and luxurious atmosphere. Hand-poured with natural soy wax for a clean, long-lasting burn, creating a perfect ambiance for relaxation or meditation.",
+    price: 120,
+    image: [product1, bannerImage2, bannerImage3, bannerImage1],
+    features: [
+      "Over 50 hours of long-lasting burn time",
+      "Eco-friendly natural soy wax blend",
+      "Subtle, sophisticated amber and wood fragrance",
+    ],
   },
   {
     id: "3",
-    name: "Lavender Dream",
+    name: "Vanilla Bean Magic",
     description:
       "A calming blend of French lavender and vanilla bean, perfect for unwinding.",
     price: 100,
-    image: [bannerImage1],
+    image: [bannerImage2, product1, bannerImage1, bannerImage3],
+    features: [
+      "Over 50 hours of long-lasting burn time",
+      "Eco-friendly natural soy wax blend",
+      "Subtle, sophisticated amber and wood fragrance",
+    ],
   },
   {
     id: "4",
-    name: "Lavender Dream",
+    name: "Citrus Sunrise",
     description:
       "A calming blend of French lavender and vanilla bean, perfect for unwinding.",
     price: 100,
-    image: [bannerImage1],
+    image: [bannerImage3, bannerImage1, product1, bannerImage2],
+    features: [
+      "Over 50 hours of long-lasting burn time",
+      "Eco-friendly natural soy wax blend",
+      "Subtle, sophisticated amber and wood fragrance",
+    ],
   },
   {
     id: "5",
-    name: "Lavender Dream",
+    name: "Midnight Rose",
     description:
       "A calming blend of French lavender and vanilla bean, perfect for unwinding.",
     price: 100,
-    image: [bannerImage1],
+    image: [bannerImage1, product1, bannerImage3, bannerImage2],
+    features: [
+      "Over 50 hours of long-lasting burn time",
+      "Eco-friendly natural soy wax blend",
+      "Subtle, sophisticated amber and wood fragrance",
+    ],
   },
   {
     id: "6",
-    name: "Lavender Dream",
+    name: "Ocean Breeze",
     description:
       "A calming blend of French lavender and vanilla bean, perfect for unwinding.",
     price: 100,
-    image: [bannerImage1],
+    image: [product1, bannerImage1, bannerImage2, bannerImage3],
+    features: [
+      "Over 50 hours of long-lasting burn time",
+      "Eco-friendly natural soy wax blend",
+      "Subtle, sophisticated amber and wood fragrance",
+    ],
   },
 ];
