8dabfd9633b2df4d74449930a633697f7f1d9592
arbaz9234 <arbaz9234@gmail.com>
2026-04-10 23:48:56 +0530

    Updated App routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ export default function App() {
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] xl2:!px-[15vw]  xxl2:!px-[24vw]">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/wick-and-aura" element={<Home />} />
+        <Route path="/wick-and-aura/" element={<Home />} />
       </Routes>
     </div>
   );
